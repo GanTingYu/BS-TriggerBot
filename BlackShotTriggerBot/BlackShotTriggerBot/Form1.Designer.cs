6c305944f3b7112d0703ac8fdfce9c3b0024f2bf
GanTingYu <gantingyu@gmail.com>
2021-06-29 21:02:04 +0800

    Update_VisualChangeWhenOn

--- BlackShotTriggerBot/BlackShotTriggerBot/Form1.Designer.cs
+++ BlackShotTriggerBot/BlackShotTriggerBot/Form1.Designer.cs
@@ -30,25 +30,10 @@ namespace BlackShotTriggerBot
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.Customcursor_checkBox = new System.Windows.Forms.CheckBox();
             this.Triggerbot_checkBox = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // Customcursor_checkBox
-            // 
-            this.Customcursor_checkBox.AutoSize = true;
-            this.Customcursor_checkBox.BackColor = System.Drawing.Color.Transparent;
-            this.Customcursor_checkBox.Font = new System.Drawing.Font("Comic Sans MS", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Customcursor_checkBox.ForeColor = System.Drawing.Color.Red;
-            this.Customcursor_checkBox.Location = new System.Drawing.Point(63, 197);
-            this.Customcursor_checkBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.Customcursor_checkBox.Name = "Customcursor_checkBox";
-            this.Customcursor_checkBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.Customcursor_checkBox.Size = new System.Drawing.Size(270, 49);
-            this.Customcursor_checkBox.TabIndex = 1;
-            this.Customcursor_checkBox.Text = "Custom Cursor";
-            this.Customcursor_checkBox.UseVisualStyleBackColor = false;
-            this.Customcursor_checkBox.CheckedChanged += new System.EventHandler(this.Customcursor_checkBox_CheckedChanged);
             // 
             // Triggerbot_checkBox
             // 
@@ -56,7 +41,7 @@ namespace BlackShotTriggerBot
             this.Triggerbot_checkBox.BackColor = System.Drawing.Color.Transparent;
             this.Triggerbot_checkBox.Font = new System.Drawing.Font("Comic Sans MS", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Triggerbot_checkBox.ForeColor = System.Drawing.Color.Red;
-            this.Triggerbot_checkBox.Location = new System.Drawing.Point(63, 106);
+            this.Triggerbot_checkBox.Location = new System.Drawing.Point(51, 27);
             this.Triggerbot_checkBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Triggerbot_checkBox.Name = "Triggerbot_checkBox";
             this.Triggerbot_checkBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
@@ -66,30 +51,41 @@ namespace BlackShotTriggerBot
             this.Triggerbot_checkBox.UseVisualStyleBackColor = false;
             this.Triggerbot_checkBox.CheckedChanged += new System.EventHandler(this.Triggerbot_checkBox_CheckedChanged);
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.Triggerbot_checkBox);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(338, 81);
+            this.panel1.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::BlackShotTriggerBot.Properties.Resources.bswallpaper;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(879, 682);
-            this.Controls.Add(this.Triggerbot_checkBox);
-            this.Controls.Add(this.Customcursor_checkBox);
+            this.ClientSize = new System.Drawing.Size(338, 94);
+            this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
             this.Name = "Form1";
-            this.Text = "Blackshot Hack";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Blackshot Hack - Mr.G";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.CheckBox Customcursor_checkBox;
         private System.Windows.Forms.CheckBox Triggerbot_checkBox;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- BlackShotTriggerBot/BlackShotTriggerBot/Form1.cs
+++ BlackShotTriggerBot/BlackShotTriggerBot/Form1.cs
@@ -1,13 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
+using System.Runtime.InteropServices;   //For GetAsyncKeyState
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using AutoItX3Lib;  //autoit .dll library
 
@@ -15,61 +9,104 @@ namespace BlackShotTriggerBot
 {
     public partial class Form1 : Form
     {
-        [DllImport("user32.dll")]   //Library to emulate keyboard keys stokes
-        static extern short GetAsyncKeyState(Keys vKey);
+        [DllImport("user32.dll")]   //Library to emulate keyboard keys stokes & finding window handle
+        public static extern short GetAsyncKeyState(Keys vKey);        //Get keyboard key strokes
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string IpClassName, string IpWindowName);    //Find Window Application
+        [DllImport("user32.dll")]
+        public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);     //Set Window Long
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int GetWindowLong(IntPtr hWnd, int nIndex);                    //Get Window Long
+
         object pixel;
         int newCol;
-        int[] coor = { 960, 540 };   //coordinate for 1920x1080
+        int[] coor = { 960, 540 };   //coordinate for laptop screen size 1920x1080
         AutoItX3 au3 = new AutoItX3();
+        bool TBCheckBoxFlag = false;
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        //Load the Form gui
         private void Form1_Load(object sender, EventArgs e)
         {
-            Thread mn = new Thread(Main) {IsBackground=true};
-            mn.Start();
+            CheckForIllegalCrossThreadCalls = false; //Prevent "illegal cross thread call" problem when changing the checkbox tick ui
+            //Make background transparent
+            this.BackColor = Color.Wheat;
+            this.TransparencyKey = Color.Wheat;
+            this.FormBorderStyle = FormBorderStyle.None;    //Remove program window border
+            this.WindowState = FormWindowState.Maximized; 
+            this.TopMost = true;    //Set the program window at top most
+
+
+            //Enable user to click through this program
+            int initialStyle = GetWindowLong(this.Handle, -20);
+            SetWindowLong(this.Handle,-20,initialStyle|0x8000|0x20);
+            
+
+            /*
+            GetWindowRect(Handle, out rect);
+            this.Size = new Size(rect.right - rect.left, rect.bottom - rect.top);
+
+            this.Left = rect.left;
+            this.Top = rect.top;
+            */
+
+            Thread mn = new Thread(Main) {IsBackground=true};   //Create new thread called "main", the thread will be closed when program closed
+            mn.Start(); //Start the thread
         }
 
         private void Main()
         {
-            
+            while (true)
+            {
+                if (GetAsyncKeyState(Keys.X) < 0)   //Set the keyboard hotkey to start/stop the bot
+                {
+                    TBCheckBoxFlag = !TBCheckBoxFlag;
+                    Triggerbot_checkBox.Checked = !Triggerbot_checkBox.Checked; //Change the checkbox tick ui based on ON/OFF of the bot
+                    Thread.Sleep(200);
+                }
+                else
+                {
+                    if (TBCheckBoxFlag == false)   //When triggerbot checkbox is not true
+                    {
+                        //Do nothing
+                    }
+                    else //When triggerbot checkbox is true
+                    {
+                        newCol = GrabColor();   //Grab color from coord: 960, 540 
+                        if (newCol == 16711680) //If the color is red, then left click with a delay time
+                        {
+                            au3.MouseClick("LEFT");
+                            Thread.Sleep(30);
+                        }
+                    }
+                }
+                
+            }
         }
 
-        //Triggerbot start
+        //Grab screen pixel color function
         int GrabColor()
         {
             pixel = au3.PixelGetColor(coor[0], coor[1]);
             return Int32.Parse(pixel.ToString());
         }
-
+        
         private void Triggerbot_checkBox_CheckedChanged(object sender, EventArgs e)
         {
-            
-            if (Triggerbot_checkBox.Checked)
+            if(Triggerbot_checkBox.Checked)
             {
-                while (true)
-                {
-               
-                    newCol = GrabColor();
-                    if (newCol  == 16711680)
-                    {
-                        au3.MouseClick("LEFT");
-                        Thread.Sleep(1);
-                    }
-                    
-                    //Thread.Sleep(1);
-                }
-                
+                Triggerbot_checkBox.ForeColor = System.Drawing.Color.Green;
+            }
+            else
+            {
+                Triggerbot_checkBox.ForeColor = System.Drawing.Color.Red;
             }
         }
-        //Triggerbot end
-        private void Customcursor_checkBox_CheckedChanged(object sender, EventArgs e)
-        {
 
-        }
-
-        
-    }   
+    }
 }
